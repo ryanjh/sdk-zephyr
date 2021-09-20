f5c58822f8c0c278fb759c8881483717c9930587
Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>
2021-09-20 14:26:32 +0200

    [nrf noup] modules: hal_nordic: nrfx: add missing definitions for cache
    add missing definitions for cache
    
    Signed-off-by: Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>
    (cherry picked from commit f922e9822a586aa97b45e6a974936be893ad92a0)
    (cherry picked from commit de7a1c88f7678164d795b2764e6acd85d1ab30c1)

--- modules/hal_nordic/nrfx/nrfx_glue.h
+++ modules/hal_nordic/nrfx/nrfx_glue.h
@@ -251,6 +251,24 @@ void nrfx_busy_wait(uint32_t usec_to_wait);
 
 //------------------------------------------------------------------------------
 
+/**
+ * @brief Macro for flushing cache lines associated with the specified buffer.
+ *
+ * @param[in] p_buffer Pointer to the buffer.
+ * @param[in] size     Size of the buffer.
+ */
+#define NRFY_CACHE_FLUSH(p_buffer, size)
+
+/**
+ * @brief Macro for invalidating cache lines associated with the specified buffer.
+ *
+ * @param[in] p_buffer Pointer to the buffer.
+ * @param[in] size     Size of the buffer.
+ */
+#define NRFY_CACHE_INVALIDATE(p_buffer, size)
+
+//------------------------------------------------------------------------------
+
 /** @brief Bitmask that defines DPPI channels that are reserved for use outside of the nrfx library. */
 #define NRFX_DPPI_CHANNELS_USED   (NRFX_PPI_CHANNELS_USED_BY_BT_CTLR |    \
 				   NRFX_PPI_CHANNELS_USED_BY_802154_DRV | \
