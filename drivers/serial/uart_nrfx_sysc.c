402f0940947f28910a74656c834b300880ba2024
Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>
2021-10-04 17:41:18 +0200

    [nrf noup] drivers: serial: add uart_nrfx_sysc
    Also includes rework of Kconfig.nrfx and adding UARTE130-135
    
    NRFX-1179
    
    Signed-off-by: Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>
    (cherry picked from commit b8957a3bc4a58619161bbb6e01462fa38e700a81)
    (cherry picked from commit 4b0be56e7e156dc1996f8f2818ddfe9c83108e34)

---
+++ drivers/serial/uart_nrfx_sysc.c
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <drivers/uart.h>
+#include <hal/nrf_uarte.h>
+
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
+#error "NO SUPPORT FOR IRQ DRIVEN"
+#endif
+
+struct sysc_nrfx_config {
+	NRF_UARTE_Type *reg;
+};
+
+static char dma_buf;
+
+static inline const struct sysc_nrfx_config *get_dev_config(const struct device *dev)
+{
+	return dev->config;
+}
+
+static inline NRF_UARTE_Type *get_uarte_instance(const struct device *dev)
+{
+	const struct sysc_nrfx_config *config = get_dev_config(dev);
+
+	return config->reg;
+}
+
+static void sysc_poll_out(const struct device *dev, unsigned char c)
+{
+	NRF_UARTE_Type *uarte = get_uarte_instance(dev);
+
+	dma_buf = c;
+	nrf_uarte_tx_buffer_set(uarte, &dma_buf, 1);
+
+	nrf_uarte_event_clear(uarte, NRF_UARTE_EVENT_ENDTX);
+
+	nrf_uarte_task_trigger(uarte, NRF_UARTE_TASK_STARTTX);
+
+	while(!nrf_uarte_event_check(uarte, NRF_UARTE_EVENT_ENDTX))
+	{}
+
+	nrf_uarte_event_clear(uarte, NRF_UARTE_EVENT_ENDTX);
+}
+
+static int sysc_poll_in(const struct device *dev, unsigned char *c)
+{
+        return -1;
+}
+
+static const struct uart_driver_api sysc_driver_api = {
+	.poll_in	= sysc_poll_in,
+	.poll_out	= sysc_poll_out,
+	.err_check	= NULL,
+};
+
+static int sysc_init(const struct device *dev)
+{
+	NRF_UARTE_Type *uarte = get_uarte_instance(dev);
+	nrf_uarte_enable(uarte);
+	return 0;
+}
+
+#define UARTE(idx)	DT_NODELABEL(uart##idx)
+
+#define UART_NRF_SYSC_DEVICE(idx)				      \
+	static const struct  sysc_nrfx_config sysc_##idx##_config = { \
+		.reg = (NRF_UARTE_Type *)DT_REG_ADDR(UARTE(idx)),     \
+	};							      \
+	DEVICE_DT_DEFINE(UARTE(idx),				      \
+			 sysc_init,				      \
+			 device_pm_control_nop,			      \
+			 NULL,					      \
+			 &sysc_##idx##_config,			      \
+			 PRE_KERNEL_1,				      \
+			 CONFIG_KERNEL_INIT_PRIORITY_DEVICE,	      \
+			 &sysc_driver_api);
+
+#ifdef CONFIG_UART_130_NRF_UARTE
+UART_NRF_SYSC_DEVICE(130);
+#endif
+
+#ifdef CONFIG_UART_131_NRF_UARTE
+UART_NRF_SYSC_DEVICE(131);
+#endif
+
+#ifdef CONFIG_UART_132_NRF_UARTE
+UART_NRF_SYSC_DEVICE(132);
+#endif
+
+#ifdef CONFIG_UART_133_NRF_UARTE
+UART_NRF_SYSC_DEVICE(133);
+#endif
+
+#ifdef CONFIG_UART_134_NRF_UARTE
+UART_NRF_SYSC_DEVICE(134);
+#endif
+
+#ifdef CONFIG_UART_135_NRF_UARTE
+UART_NRF_SYSC_DEVICE(135);
+#endif
+
+#ifdef CONFIG_UART_136_NRF_UARTE
+UART_NRF_SYSC_DEVICE(136);
+#endif
