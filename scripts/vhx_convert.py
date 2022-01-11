436efedfb7cd8c98f4c61f14e558efb5b2e691c5
Stephen Stauts <stephen.stauts@nordicsemi.no>
2022-01-11 15:40:56 +0100

    [nrf noup] scripts: vhx_convert: Allow for memory width specification
    Adds an option to specify memory "line" width for the vhx file. This is
    to accommodate some regions of emulator memory that are 64 bits wide.
    MRAM (usually the default use for this script) has line widths of 128
    and is kept as the default value.
    
    Ref: NCSDK-NONE
    
    Signed-off-by: Stephen Stauts <stephen.stauts@nordicsemi.no>
    (cherry picked from commit 65809dbd23c04131ec7d8f7e7a48375403e5b394)
    (cherry picked from commit ebbfc0aae1fb21957fea65207cc2fc5905510fa8)

--- scripts/vhx_convert.py
+++ scripts/vhx_convert.py
@@ -6,44 +6,55 @@
 #
 
 import argparse
-import sys
 from intelhex import IntelHex
 
+SUPPORTED_BYTE_WIDTHS = [64, 128]
 
-def convert_bin2vhx(infile, outfile):
-    with open(infile, "rb") as infile:
-        with open(outfile, "w") as outfile:
+
+def convert_bin2vhx(infile, outfile, width):
+    byte_width = width//8
+    if width not in SUPPORTED_BYTE_WIDTHS:
+        raise ValueError(f"Invalid byte width; must be {SUPPORTED_BYTE_WIDTHS},"
+                         f" got {byte_width}.")
+
+    with open(infile, "rb") as in_file:
+        with open(outfile, "w") as out_file:
             while True:
-                s = infile.read(16)
+                s = in_file.read(byte_width)
                 if not s:
                     break
                 s = bytearray(s)
                 s.reverse()
                 o = ''.join(["%.2X" % i for i in s])
 
-                outfile.write(o)
-                outfile.write('\n')
-    infile.close()
-    outfile.close()
+                out_file.write(o)
+                out_file.write('\n')
+    in_file.close()
+    out_file.close()
 
 
-def convert_hex2vhx(infile, outfile):
+def convert_hex2vhx(infile, outfile, width):
+    byte_width = width//8
+    if width not in SUPPORTED_BYTE_WIDTHS:
+        raise ValueError(f"Invalid byte width; must be {SUPPORTED_BYTE_WIDTHS},"
+                         f" got {byte_width}.")
+
     def bin_reader(input_array):
-        for i in range(0, len(input_array), 16):
-            yield input_array[i:i+16]
+        for i in range(0, len(input_array), byte_width):
+            yield input_array[i:i+byte_width]
 
     ih = IntelHex(infile)
     b = ih.tobinarray()
 
-    with open(outfile, "w") as outfile:
+    with open(outfile, "w") as out_file:
         for s in bin_reader(b):
             s = bytearray(s)
             s.reverse()
             o = ''.join(["%.2X" % i for i in s])
 
-            outfile.write(o)
-            outfile.write('\n')
-    outfile.close()
+            out_file.write(o)
+            out_file.write('\n')
+    out_file.close()
 
 
 def main():
@@ -54,12 +65,14 @@ def main():
     parser.add_argument("-f", "--input-format", type=str,
         choices=["bin", "hex"], default="bin",
         help="Input file format (default, bin)")
+    parser.add_argument("-w", "--width", type=int,
+        choices=SUPPORTED_BYTE_WIDTHS, default=128,
+        help="Line width of the VHX output")
     args = parser.parse_args()
-
     if args.input_format == "bin":
-        convert_bin2vhx(args.file, args.output)
+        convert_bin2vhx(args.file, args.output, args.width)
     elif args.input_format == "hex":
-        convert_hex2vhx(args.file, args.output)
+        convert_hex2vhx(args.file, args.output, args.width)
 
 
 if __name__ == "__main__":
