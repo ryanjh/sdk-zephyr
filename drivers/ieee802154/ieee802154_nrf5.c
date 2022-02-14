d0c48a82ec05003911a5be2f93af186658a14fb0
Sebastian Głąb <sebastian.glab@nordicsemi.no>
2022-02-14 11:36:16 +0100

    [nrf noup] samples: drivers: Change board nRF5440 to nRF5420
    Change board nRF5440 to nRF5420
    
    Signed-off-by: Sebastian Głąb <sebastian.glab@nordicsemi.no>
    (cherry picked from commit 19ac1440e7828eedf334fa9b91c847c6482cf77f)

--- drivers/ieee802154/ieee802154_nrf5.c
+++ drivers/ieee802154/ieee802154_nrf5.c
@@ -77,7 +77,7 @@ static struct nrf5_802154_data nrf5_data;
 #else
 #if defined(CONFIG_SOC_NRF5340_CPUAPP) || defined(CONFIG_SOC_NRF5340_CPUNET)
 #define EUI64_ADDR (NRF_FICR->INFO.DEVICEID)
-#elif defined(CONFIG_SOC_NRF5440_CPUAPP) || defined(CONFIG_SOC_NRF5440_CPUNET)
+#elif defined(CONFIG_SOC_NRF5420_CPUAPP) || defined(CONFIG_SOC_NRF5420_CPUNET)
 /* TODO: Replace with the DEVICE ID value on nRF54. This value is not available
  * in the current FICR structuretype definition.
  */
