12cad85e3d01ddaec1e7a051b7f730c1d97de89e
Rafał Kuźnia <rafal.kuznia@nordicsemi.no>
2022-04-11 10:06:27 +0200

    [nrf noup] samples: boards: nrf: ieee802154: Fixes for ipc_service
    The remote must wait for the reset flag to be cleared before
    progressing further.
    The heap must be increased for the initialization to be performed
    successfuly.
    
    Signed-off-by: Rafał Kuźnia <rafal.kuznia@nordicsemi.no>
    (cherry picked from commit 2cd3018ee04c40490f7ad73dad893599d5e1a167)
    (cherry picked from commit ef1205e8def40b927a15f0fcb7f85d559cee5245)

--- samples/boards/nrf/ieee802154/802154_rpmsg/src/main.c
+++ samples/boards/nrf/ieee802154/802154_rpmsg/src/main.c
@@ -6,6 +6,7 @@
 
 #include <zephyr/sys/__assert.h>
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
 
 #include <nrf_802154_serialization_error.h>
 
@@ -19,3 +20,28 @@ void nrf_802154_sl_fault_handler(uint32_t id, int32_t line, const char *err)
 {
 	__ASSERT(false, "module_id: %u, line: %d, %s", id, line, err);
 }
+
+#if IS_ENABLED(CONFIG_SOC_PLATFORM_HALTIUM)
+
+int check_status_flag(const struct device *arg)
+{
+	volatile uint8_t *p = (volatile uint8_t *) DT_REG_ADDR(DT_NODELABEL(gram00));
+
+	/*
+	 * Resetting the emulator doesn't also clear the memory. This means
+	 * that from one run to another, the synchronization flag in the shared
+	 * memory used by OpenAMP retains the old value. This causes the remote
+	 * endpoint to wrongfully think that the host has already started
+	 * causing mayhem and desperation for the poor dev soul trying to debug
+	 * this. Reset the flag and make the remote core waiting until this is
+	 * correctly set again before starting the sample.
+	 */
+
+	while (*p != 0x0)
+		;
+
+	return 0;
+}
+SYS_INIT(check_status_flag, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
+
+#endif
