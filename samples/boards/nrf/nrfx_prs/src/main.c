99055a50cfb2a7aaf942c0e3344ac0225ea0a4a7
Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>
2022-04-27 16:51:14 +0200

    [nrf noup] samples: boards: nrf: nrfx_prs: align to pinctrl and NRFY
    nrfx_prs: align to pinctrl and NRFY
    
    Signed-off-by: Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>
    (cherry picked from commit eac2374c4c3e0be565dd815e606b093e6e99161d)
    (cherry picked from commit 3a7fb9ac9ff5e9a61c9382c31d30b4d4f6abe3bc)

--- samples/boards/nrf/nrfx_prs/src/main.c
+++ samples/boards/nrf/nrfx_prs/src/main.c
@@ -144,6 +144,13 @@ static bool switch_to_spim(void)
 	spim_config.nrfy_config.frequency = NRF_SPIM_FREQ_1M;
 	spim_config.miso_pull = NRF_GPIO_PIN_PULLDOWN;
 
+       ret = pinctrl_apply_state(PINCTRL_DT_DEV_CONFIG_GET(SPIM_NODE),
+                                 PINCTRL_STATE_DEFAULT);
+       if (ret < 0) {
+               return ret;
+       }
+
+
 	err = nrfx_spim_init(&spim, &spim_config, spim_handler, NULL);
 	if (err != NRFX_SUCCESS) {
 		printk("nrfx_spim_init() failed: 0x%08x\n", err);
@@ -214,9 +221,9 @@ static bool switch_to_uarte(void)
 	nrfx_uarte_config_t uarte_config = NRFX_UARTE_DEFAULT_CONFIG(
 		NRF_UARTE_PSEL_DISCONNECTED,
 		NRF_UARTE_PSEL_DISCONNECTED);
-	uarte_config.baudrate = NRF_UARTE_BAUDRATE_1000000;
+	uarte_config.nrfy_config.baudrate = NRF_UARTE_BAUDRATE_1000000;
 	uarte_config.skip_gpio_cfg = true;
-	uarte_config.skip_psel_cfg = true;
+	uarte_config.nrfy_config.skip_psel_cfg = true;
 
 	ret = pinctrl_apply_state(PINCTRL_DT_DEV_CONFIG_GET(UARTE_NODE),
 				  PINCTRL_STATE_DEFAULT);
