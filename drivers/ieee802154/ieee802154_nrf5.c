d16768b89fc378fe11b853f20bac7f736866100b
Adam Wojasinski <adam.wojasinski@nordicsemi.no>
2022-06-27 09:43:00 +0200

    [nrf noup] drivers: Align to MDK 8.46
    This commit aligns drivers to three new targets
    that haltium has been split to:
    - liliumfp1
    - liliumsoc1
    - nrf9230
    
    Jira: NRFX-1913
    
    Signed-off-by: Adam Wojasinski <adam.wojasinski@nordicsemi.no>
    (cherry picked from commit e94a3751ea53e3e08cde7949e5b051270b64f28e)
    (cherry picked from commit 0dca6affbe26a7494df133f2e8e4a5783098d943)

--- drivers/ieee802154/ieee802154_nrf5.c
+++ drivers/ieee802154/ieee802154_nrf5.c
@@ -77,7 +77,8 @@ static struct nrf5_802154_data nrf5_data;
 #else
 #if defined(CONFIG_SOC_NRF5340_CPUAPP) || defined(CONFIG_SOC_NRF5340_CPUNET)
 #define EUI64_ADDR (NRF_FICR->INFO.DEVICEID)
-#elif defined(CONFIG_SOC_NRF5420_CPUAPP) || defined(CONFIG_SOC_NRF5420_CPUNET)
+#elif defined(CONFIG_SOC_NRF5420_SOC1_CPUAPP) || defined(CONFIG_SOC_NRF5420_CPUAPP) || \
+	defined(CONFIG_SOC_NRF5420_SOC1_CPUNET) || defined(CONFIG_SOC_NRF5420_CPUNET)
 /* TODO: Replace with the DEVICE ID value on nRF54. This value is not available
  * in the current FICR structuretype definition.
  */
