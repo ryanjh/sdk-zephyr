45e8b661e50a40f44d49f1cc76bd9e90f27b2a1b
Marcin Gasiorek <marcin.gasiorek@nordicsemi.no>
2023-02-21 14:18:57 +0100

    [nrf noup] drivers: temp: Improve temperature sensor driver
    Fixed building issue - added missing dependency in the Kconfig.
    Trigger event handler has been moved from workqueue to thread (similar like in another zephyr drivers).
    Fixed coding standard issues.
    Handling error code from NRFS in temp_nrfs_sample_fetch() function.
    Updated unit tests and removed invalid UT.
    
    Signed-off-by: Marcin Gasiorek <marcin.gasiorek@nordicsemi.no>
    (cherry picked from commit 0c095a6c742b568808f84f54a090ef1ac3f575e0)

--- drivers/sensor/nrf5/temp_nrfs.c
+++ drivers/sensor/nrf5/temp_nrfs.c
@@ -4,9 +4,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "kernel.h"
 #define DT_DRV_COMPAT nordic_nrf_temp_nrfs
 
+#include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
 #include <hal/nrf_temp.h>
@@ -16,19 +16,16 @@
 
 LOG_MODULE_REGISTER(temp_nrfs, CONFIG_SENSOR_LOG_LEVEL);
 
-#if CONFIG_TEMP_NRF5_TRIGGER
+#ifdef CONFIG_TEMP_NRF5_TRIGGER
 
-#define MS_IN_SECOND			  1000
-#define HZ_TO_TICK_DELAY_MS(frequency_hz) (MS_IN_SECOND / frequency_hz)
-#define CLIP_TO_RANGE(x, low, high)                                            \
-	(((x) > (high)) ? (high) : (((x) < (low)) ? (low) : (x)))
+#define HZ_TO_TICK_DELAY_MS(frequency_hz) (MSEC_PER_SEC / frequency_hz)
 
-#define DEFAULT_SENSOR_ATTRIBUTES                                              \
-	(struct sensor_attributes)                                             \
-	{                                                                      \
-		.sampling_freq = {10, 0},                                      \
-                .up_threshold = {INT32_MAX, 0},                                \
-		.low_threshold = {INT32_MIN, 0}                                \
+#define DEFAULT_SENSOR_ATTRIBUTES	   \
+	(struct sensor_attributes)	   \
+	{				   \
+		.sampling_freq = { 1, 0 }, \
+		.up_threshold = { 25, 0 }, \
+		.low_threshold = { 0, 0 }  \
 	}
 
 struct sensor_attributes {
@@ -37,42 +34,57 @@ struct sensor_attributes {
 	struct sensor_value low_threshold;
 };
 
-struct temp_trigger {
-	struct device *dev;
-	struct sensor_trigger *trigger;
-	sensor_trigger_handler_t handler;
-	struct k_work work;
-	struct k_work_q workq;
-	K_KERNEL_STACK_MEMBER(stack,
-			      CONFIG_TEMP_NRF5_TRIGGER_THREAD_STACK_SIZE);
-};
-
-#endif // CONFIG_TEMP_NRF5_TRIGGER
+#endif /* CONFIG_TEMP_NRF5_TRIGGER */
 
 struct temp_nrfs_data {
-	struct k_sem ipc_sync_sem;
+	struct k_sem nrfs_measure_done_sem;
 	struct k_mutex mutex;
 	int32_t raw_temp;
 	int erc;
-#if CONFIG_TEMP_NRF5_TRIGGER
+#ifdef CONFIG_TEMP_NRF5_TRIGGER
+	struct device *dev;
+	K_KERNEL_STACK_MEMBER(thread_stack, CONFIG_TEMP_NRF5_TRIGGER_THREAD_STACK_SIZE);
+	struct sensor_trigger trigger;
+	sensor_trigger_handler_t handler;
+
+	struct k_thread thread;
+	struct k_sem nrfs_event_change_sem;
+
 	struct sensor_attributes attr;
-	struct temp_trigger trigger;
-#endif
+#endif /* CONFIG_TEMP_NRF5_TRIGGER */
 };
 
 static struct temp_nrfs_data temp_nrfs_driver;
 
+#ifdef CONFIG_TEMP_NRF5_TRIGGER
 static int32_t sensor_value_to_raw_temp(struct sensor_value *value)
 {
 	const uint8_t decimal_places_offset = 100;
-	return (int32_t)(sensor_value_to_double(value) * decimal_places_offset);
+
+	return nrfs_temp_to_raw((sensor_value_to_double(value) * decimal_places_offset));
 }
 
-#if CONFIG_TEMP_NRF5_TRIGGER
-static void sensor_trigger_work(struct k_work *work)
+static void nrfs_temp_thread_cb(const struct device *dev)
 {
-	struct temp_trigger *trig = CONTAINER_OF(work, struct temp_trigger, work);
-	trig->handler(trig->dev, trig->trigger);
+	struct temp_nrfs_data *data = (struct temp_nrfs_data *)dev->data;
+
+	k_mutex_lock(&data->mutex, K_FOREVER);
+	if (data->handler) {
+		data->handler(dev, &data->trigger);
+	}
+	k_mutex_unlock(&data->mutex);
+}
+
+static void nrfs_temp_thread(void *drv_data, void *p2, void *p3)
+{
+	ARG_UNUSED(p2);
+	ARG_UNUSED(p3);
+
+	struct temp_nrfs_data *data = (struct temp_nrfs_data *)drv_data;
+	while (true) {
+		k_sem_take(&data->nrfs_event_change_sem, K_FOREVER);
+		nrfs_temp_thread_cb(data->dev);
+	}
 }
 
 static int temp_nrfs_sensor_trigger_set(const struct device *dev,
@@ -82,28 +94,25 @@ static int temp_nrfs_sensor_trigger_set(const struct device *dev,
 	struct temp_nrfs_data *data = (struct temp_nrfs_data *)dev->data;
 	struct sensor_attributes *attr = &data->attr;
 
-	if (trig == NULL || (trig->chan != SENSOR_CHAN_ALL &&
-			     trig->chan != SENSOR_CHAN_DIE_TEMP)) {
+	if (!trig || (trig->chan != SENSOR_CHAN_ALL &&
+		      trig->chan != SENSOR_CHAN_DIE_TEMP)) {
 		return -ENOTSUP;
 	}
 
-	if (data->trigger.handler && (data->trigger.handler != handler) && (handler != NULL)) {
+	if (data->handler && (data->handler != handler) && handler) {
 		return -EBUSY;
 	}
 
 	switch (trig->type) {
 	case SENSOR_TRIG_THRESHOLD: {
 		k_mutex_lock(&data->mutex, K_FOREVER);
-		k_work_cancel(&data->trigger.work);
-		data->trigger.trigger = (struct sensor_trigger *)trig;
-		data->trigger.handler = handler;
+		data->trigger = *trig;
+		data->handler = handler;
 		k_mutex_unlock(&data->mutex);
-
-		data->trigger.dev = (struct device *)dev;
 
 		nrfs_err_t err;
 		if (handler) {
-			uint16_t measure_rate_ms = CLIP_TO_RANGE(
+			uint16_t measure_rate_ms = CLAMP(
 				HZ_TO_TICK_DELAY_MS(
 					sensor_value_to_double(&attr->sampling_freq)),
 				0, UINT16_MAX);
@@ -118,11 +127,11 @@ static int temp_nrfs_sensor_trigger_set(const struct device *dev,
 		}
 
 		switch (err) {
-			case NRFS_SUCCESS: return 0;
-			case NRFS_ERR_INVALID_STATE: return -ENOTCONN;
-			case NRFS_ERR_IPC: return -EIO;
-			default:
-				return -ENOTSUP;
+		case NRFS_SUCCESS: return 0;
+		case NRFS_ERR_INVALID_STATE: return -ENOTCONN;
+		case NRFS_ERR_IPC: return -EIO;
+		default:
+			return -ENOTSUP;
 		}
 		break;
 	}
@@ -139,11 +148,12 @@ static int temp_nrfs_sensor_attr_set(const struct device *dev,
 				     const struct sensor_value *val)
 {
 	struct temp_nrfs_data *data = (struct temp_nrfs_data *)dev->data;
+
 	if (chan != SENSOR_CHAN_ALL && chan != SENSOR_CHAN_DIE_TEMP) {
 		return -ENOTSUP;
 	}
 
-	if (val == NULL) {
+	if (!val) {
 		LOG_ERR("Invalid sensor value passed to attribute");
 		return -ENOTSUP;
 	}
@@ -170,7 +180,8 @@ static int temp_nrfs_sensor_attr_set(const struct device *dev,
 	}
 	return 0;
 }
-#endif // CONFIG_TEMP_NRF5_TRIGGER
+
+#endif /* CONFIG_TEMP_NRF5_TRIGGER */
 
 static void sensor_handler(nrfs_temp_evt_t const *p_evt, void *context)
 {
@@ -180,26 +191,17 @@ static void sensor_handler(nrfs_temp_evt_t const *p_evt, void *context)
 
 	switch (p_evt->type) {
 	case NRFS_TEMP_EVT_MEASURE_DONE:
-		k_mutex_lock(&data->mutex, K_FOREVER);
 		data->erc = 0;
 		data->raw_temp = p_evt->raw_temp;
-		k_mutex_unlock(&data->mutex);
-		LOG_DBG("Temperature Measurement done");
-		k_sem_give(&data->ipc_sync_sem);
+		k_sem_give(&data->nrfs_measure_done_sem);
 		break;
-#if CONFIG_TEMP_NRF5_TRIGGER
+#ifdef CONFIG_TEMP_NRF5_TRIGGER
 	case NRFS_TEMP_EVT_CHANGE:
-		k_mutex_lock(&data->mutex, K_FOREVER);
 		data->erc = 0;
 		data->raw_temp = p_evt->raw_temp;
-		if (data->trigger.trigger && data->trigger.handler) {
-			k_work_submit_to_queue(
-				&data->trigger.workq,
-				&data->trigger.work);
-		}
-		k_mutex_unlock(&data->mutex);
+		k_sem_give(&data->nrfs_event_change_sem);
 		break;
-#endif // CONFIG_TEMP_NRF5_TRIGGER
+#endif /* CONFIG_TEMP_NRF5_TRIGGER */
 	default:
 		LOG_DBG("Temperature handler - unsupported event: 0x%x",
 			p_evt->type);
@@ -218,17 +220,27 @@ static int temp_nrfs_sample_fetch(const struct device *dev,
 	}
 
 	k_mutex_lock(&data->mutex, K_FOREVER);
-
-	if (nrfs_temp_measure_request(NULL) == NRFS_SUCCESS) {
-		k_sem_take(&data->ipc_sync_sem, K_FOREVER);
+	int nrfs_erc = nrfs_temp_measure_request(NULL);
+	switch (nrfs_erc) {
+	case NRFS_SUCCESS:
+		k_sem_take(&data->nrfs_measure_done_sem, K_FOREVER);
 
 		erc = data->erc;
 		LOG_DBG("Temperature sample: %d", data->raw_temp);
-
-	} else {
-		LOG_DBG("Request send failed");
+		break;
+	case NRFS_ERR_INVALID_STATE:
+		LOG_DBG("Backend is not ready, try again.");
+		erc = -ENOTCONN;
+		break;
+	case NRFS_ERR_IPC:
+		LOG_DBG("Measure request failed.");
+		erc = -EIO;
+		break;
+	default:
+		LOG_DBG("Measure request failed, %d", nrfs_erc);
+		erc = -ENOTSUP;
+		break;
 	}
-
 	k_mutex_unlock(&data->mutex);
 
 	return erc;
@@ -248,42 +260,41 @@ static int temp_nrfs_channel_get(const struct device *dev,
 	val->val1 = uval / 100;
 	val->val2 = (uval % 100) * 10000;
 
-	LOG_DBG("Temperature:%d.%d[C]", val->val1, val->val2);
+	LOG_DBG("Temperature: %d.%d[C]", val->val1, val->val2);
 
 	return 0;
 }
 
 static const struct sensor_driver_api temp_nrfs_driver_api = {
-#if CONFIG_TEMP_NRF5_TRIGGER
+#ifdef CONFIG_TEMP_NRF5_TRIGGER
 	.attr_set = temp_nrfs_sensor_attr_set,
 	.trigger_set = temp_nrfs_sensor_trigger_set,
-#endif // CONFIG_TEMP_NRF5_TRIGGER
+#endif /* CONFIG_TEMP_NRF5_TRIGGER */
 	.sample_fetch = temp_nrfs_sample_fetch,
 	.channel_get = temp_nrfs_channel_get
 };
 
 static int temp_nrfs_init(const struct device *dev)
 {
-	struct temp_nrfs_data *data = (struct temp_nrfs_data *)dev->data;
+	struct temp_nrfs_data *dev_data = (struct temp_nrfs_data *)dev->data;
 
 	LOG_DBG("nrfs temp sensor init.");
 
-	k_sem_init(&data->ipc_sync_sem, 0, 1);
-	k_mutex_init(&data->mutex);
+	k_sem_init(&dev_data->nrfs_measure_done_sem, 0, K_SEM_MAX_LIMIT);
+	k_mutex_init(&dev_data->mutex);
 
-#if CONFIG_TEMP_NRF5_TRIGGER
-	data->attr = DEFAULT_SENSOR_ATTRIBUTES;
+#ifdef CONFIG_TEMP_NRF5_TRIGGER
+	k_sem_init(&dev_data->nrfs_event_change_sem, 0, K_SEM_MAX_LIMIT);
+	dev_data->attr = DEFAULT_SENSOR_ATTRIBUTES;
+	dev_data->dev = (struct device *)dev;
 
-	k_work_queue_init(&data->trigger.workq);
-	k_work_queue_start(
-		&data->trigger.workq,
-		data->trigger.stack,
-		K_THREAD_STACK_SIZEOF(data->trigger.stack),
-		CONFIG_TEMP_NRF5_TRIGGER_THREAD_PRIO, NULL);
-	k_thread_name_set(&data->trigger.workq.thread,
-			  "TEMP_NRF5_trigger");
-	k_work_init(&data->trigger.work, sensor_trigger_work);
-#endif // CONFIG_TEMP_NRF5_TRIGGER
+	k_thread_create(&dev_data->thread, dev_data->thread_stack,
+			CONFIG_TEMP_NRF5_TRIGGER_THREAD_STACK_SIZE,
+			nrfs_temp_thread,
+			dev_data, NULL, NULL,
+			K_PRIO_COOP(CONFIG_TEMP_NRF5_TRIGGER_THREAD_PRIO),
+			0, K_NO_WAIT);
+#endif /* CONFIG_TEMP_NRF5_TRIGGER */
 	return nrfs_temp_init(sensor_handler);
 }
 
--- tests/drivers/sensor/nrf5/src/main.c
+++ tests/drivers/sensor/nrf5/src/main.c
@@ -7,32 +7,42 @@
 #include <zephyr/ztest.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
+
 #if CONFIG_SOC_NRF54H20
-#include <internal/nrfs_backend.h>
-#include <internal/backends/nrfs_backend_ipc_service.h>
+#define TEMP_SENSOR_NODE_ID       DT_NODELABEL(temp_nrfs)
+#else
+#define TEMP_SENSOR_NODE_ID       DT_NODELABEL(temp)
 #endif
 
 void test_temp_driver_test(void)
 {
-#if CONFIG_SOC_NRF54H20
-	nrfs_backend_wait_for_connection(K_FOREVER);
-	const struct device *temp_dev = DEVICE_DT_GET(DT_NODELABEL(temp_nrfs));
-#else
- 	const struct device *temp_dev = DEVICE_DT_GET(DT_NODELABEL(temp));
-#endif
+	const struct device *temp_dev = DEVICE_DT_GET(TEMP_SENSOR_NODE_ID);
 
 	zassert_not_null(temp_dev, "Device object is NULL.");
 	zassert_true(device_is_ready(temp_dev), "Device is not ready.");
 
-	int rc = sensor_sample_fetch(temp_dev);
+	int rc = 0;
+	int retry_cnt = 0;
+	while ((rc = sensor_sample_fetch(temp_dev)) == -ENOTCONN) {
+		k_sleep(K_MSEC(1000));
+		retry_cnt++;
+		zassert_false(retry_cnt >= 3, "Backend connection fail.");
+	}
 
 	zassert_ok(rc, "Cannot fetch a sample from temperature sensor.");
 
-	struct sensor_value val = { 0, 0 };
-	rc = sensor_channel_get(temp_dev, SENSOR_CHAN_DIE_TEMP, &val);
+	struct sensor_value val;
+	sensor_value_from_double(&val, 0);
+	zassert_ok(sensor_channel_get(temp_dev, SENSOR_CHAN_DIE_TEMP, &val), "Get temperature fail.");
+	zassert_true(sensor_value_to_double(&val) > -40, "Temperature out of range %d [C]",
+		     sensor_value_to_double(&val));
 
-	zassert_ok(rc, "Get temperature fail.");
-	zassert_true(val.val1 > -40, "Temperature out of range %d.%d [C]", val.val1, val.val2);
+	zassert_ok(sensor_sample_fetch(temp_dev), "Cannot fetch a sample from temperature sensor.");
+
+	sensor_value_from_double(&val, 0);
+	zassert_ok(sensor_channel_get(temp_dev, SENSOR_CHAN_DIE_TEMP, &val), "Get temperature fail.");
+	zassert_true(sensor_value_to_double(&val) > -40, "Temperature out of range %d [C]",
+		     sensor_value_to_double(&val));
 }
 
 void test_main(void)
