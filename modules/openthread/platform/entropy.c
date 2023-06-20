d964b54a2312e204b8c4361aa3cc450bac61f1a8
Rafał Kuźnia <rafal.kuznia@nordicsemi.no>
2023-06-20 14:11:58 +0200

    Revert "[nrf noup] openthread: temporarily use PRNG if there is no entropy driver"
    This reverts commit 5da1504cff7c3556ffc194056d6712a5afff897b.
    
    Signed-off-by: Rafał Kuźnia <rafal.kuznia@nordicsemi.no>

--- modules/openthread/platform/entropy.c
+++ modules/openthread/platform/entropy.c
@@ -6,30 +6,23 @@
 
 #include <zephyr/kernel.h>
 #include <string.h>
+#include <zephyr/drivers/entropy.h>
 #include <zephyr/logging/log.h>
 
 #include <openthread/platform/entropy.h>
 
-#if defined(CONFIG_ENTROPY_HAS_DRIVER)
-#include <zephyr/drivers/entropy.h>
 #include "platform-zephyr.h"
 
-#else
-#warning OpenThread uses NOT SAFE random generator due to lack of Entropy Driver on the device!
-	/* This is temporary fix to allow working nRF54 FPGA devices with OpenThread samples.
-	 * It should be removed after adding a pseudo-entropy device
-	 * like for the net-core in ic-next.
-	 */
-#include <zephyr/random/rand32.h>
-#endif
-
 LOG_MODULE_REGISTER(net_otPlat_entropy, CONFIG_OPENTHREAD_L2_LOG_LEVEL);
+
+#if !defined(CONFIG_ENTROPY_HAS_DRIVER)
+#error OpenThread requires an entropy source for a TRNG
+#endif
 
 static const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_entropy));
 
 otError otPlatEntropyGet(uint8_t *aOutput, uint16_t aOutputLength)
 {
-#if defined(CONFIG_ENTROPY_HAS_DRIVER)
 	int err;
 
 	if ((aOutput == NULL) || (aOutputLength == 0)) {
@@ -46,8 +39,6 @@ otError otPlatEntropyGet(uint8_t *aOutput, uint16_t aOutputLength)
 		LOG_ERR("Failed to obtain entropy, err %d", err);
 		return OT_ERROR_FAILED;
 	}
-#else
-	sys_rand_get(aOutput, aOutputLength);
-#endif
+
 	return OT_ERROR_NONE;
 }
