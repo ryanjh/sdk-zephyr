4df2d35deacc2a1ce997e790605e3b779ccf2cbd
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2023-06-27 17:45:48 +0200

    [nrf noup] drviers/timer/nrf_rtc_timer: fix event_check()
    fix the driver.
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

--- drivers/timer/nrf_rtc_timer.c
+++ drivers/timer/nrf_rtc_timer.c
@@ -72,7 +72,7 @@ static void set_comparator(int32_t chan, uint32_t cyc)
 
 static bool event_check(int32_t chan)
 {
-	return nrfy_rtc_cc_get(RTC, chan);
+	return nrfy_rtc_event_check(RTC, NRF_RTC_CHANNEL_EVENT_ADDR(chan));
 }
 
 static void event_clear(int32_t chan)
